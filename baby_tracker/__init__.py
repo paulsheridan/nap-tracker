b9ac345fe469ffcd4d165d16437b740ac3cac2cd
Paul Sheridan <paul.sheridan@me.com>
2017-08-23 20:11:49 -0700

    added ini to gitignore, started process of adding auth

--- baby_tracker/__init__.py
+++ baby_tracker/__init__.py
@@ -1,10 +1,18 @@
 from pyramid.config import Configurator
 
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.config import Configurator
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+    authn_policy = AuthTktAuthenticationPolicy(
+        settings['tutorial.secret'], hashalg='sha512')
+    config.set_authentication_policy(authn_policy)
+
     config.include('cornice')
     config.include('.models')
     config.scan()
