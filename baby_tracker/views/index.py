ef0f1f65496bb6bf9e161a5dcc7dfd496fe91fc8
Paul Sheridan <paul.sheridan@bigfishgames.com>
2018-04-10 10:09:18 -0700

    moved the page views into a class, removed timers.py

--- baby_tracker/views/index.py
+++ baby_tracker/views/index.py
@@ -1,9 +1,33 @@
-from pyramid.view import view_config
+import pyramid.httpexceptions as exc
 
-@view_config(route_name='index', renderer='baby_tracker:templates/index.jinja2')
-def index_view(request):
-    return {}
+from pyramid.view import (
+    view_config,
+    view_defaults
+)
 
-@view_config(route_name='signup', renderer='baby_tracker:templates/signup.jinja2')
-def signup_view(request):
-    return {}
+@view_defaults()
+class PageView(object):
+
+    def __init__(self, request):
+        self.request = request
+        self.logged_in = request.authenticated_userid
+
+    @view_config(route_name='timers', renderer='baby_tracker:templates/timers.jinja2')
+    def timers_view(self):
+        """
+        Retrieve the timers page
+        or redirect to index if not signed in.
+        """
+        if not self.logged_in:
+            raise exc.HTTPFound(self.request.route_url("index"))
+        return {}
+
+    @view_config(route_name='index', renderer='baby_tracker:templates/index.jinja2')
+    def index_view(self):
+        """Retrieve the index page"""
+        return {}
+
+    @view_config(route_name='signup', renderer='baby_tracker:templates/signup.jinja2')
+    def signup_view(self):
+        """Retrieve the signupå page"""
+        return {}
