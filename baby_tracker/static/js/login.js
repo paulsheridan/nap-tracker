3c3a4e4f15153204eb71fafaf332b84da8e44b83
Paul Sheridan <paul.sheridan@bigfishgames.com>
2018-04-10 11:25:44 -0700

    login now working correctly and redirecting to timers

--- baby_tracker/static/js/login.js
+++ baby_tracker/static/js/login.js
@@ -1,22 +1,33 @@
 
 
 function login() {
+  console.log("logging you in")
+  var jsonData = getFormData($("#login-form"))
+  console.log(jsonData)
   $.ajax({
     type: "POST",
     url: "/login",
-    data: JSON.stringify({"email": "test@test.com", "password": "secret"}),
+    data: JSON.stringify(jsonData),
     dataType: "text",
     success: function(response){
-      console.log('logged in')
+      location.href = "timers"
     }
   });
 }
 
 $(document).ready(function() {
-  login()
-  // getLastMeal()
-  // getLastNap()
-  // $('#btn-refresh').click(function() {
-  //     getDevices();
-  // });
+  $('#btn-login').click(function() {
+      login();
+  });
 });
+
+function getFormData($form){
+  var unindexed_array = $form.serializeArray();
+  var indexed_array = {};
+
+  $.map(unindexed_array, function(n, i){
+      indexed_array[n['name']] = n['value'];
+  });
+
+  return indexed_array;
+}
